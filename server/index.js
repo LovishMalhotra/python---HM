17eb3fa1986c28afcd238062a62926f8a9d0fd5b
LovishMalhotra <lovishmalhotra2441@gmail.com>
2025-03-14 11:29:40 +0530

    changed mongodb url

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const session = require("express-session");
 const bodyParser = require("body-parser");
 const Razorpay = require("razorpay");
 const cors = require("cors");
-const MongoStore = require('connect-mongo');
+const MongoStore = require("connect-mongo");
 
 // import paymentRoute from "./routes/Payment.js";
 
@@ -28,9 +28,11 @@ const app = express();
 const port = 8080;
 
 app.use(bodyParser.json());
-app.use(cors({
-  origin: 'http://localhost:3000' 
-}));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
 app.use(
   session({
@@ -46,23 +48,20 @@ app.use(
 
 app.get("/get-session-id", (req, res) => {
   if (req.session) {
-    const sessionId = req.sessionID; 
+    const sessionId = req.sessionID;
     const userId = req.session.user;
-    console.log(userId)
-    res.json({sessionId, userId });
+    console.log(userId);
+    res.json({ sessionId, userId });
   } else {
     res.status(404).send("Session not found");
   }
 });
-
-
 
 app.use("/auth", authRouters.router);
 app.use("/jobs", jobRouters.router);
 app.use("/search", searchRouters.router);
 app.use("/query", queryRouters.router);
 app.use("/payment", paymentRoute.router);
-
 
 app.get("/payment/getKey", (req, res) =>
   res.status(200).json({ key: RAZORPAY_API_KEY })
@@ -71,7 +70,9 @@ app.get("/payment/getKey", (req, res) =>
 //Database connection
 main().catch((err) => console.log(`Unable to connect ${err}`));
 async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/IMhometutor");
+  await mongoose.connect(
+    "mongodb+srv://lovishmalhotra2441:Lovish%402441@cluster0.ipj4yj6.mongodb.net/IMhometutor?retryWrites=true&w=majority"
+  );
   console.log("Database Connected");
 }
 
